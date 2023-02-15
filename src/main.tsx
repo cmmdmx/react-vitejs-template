19f1f05fbfa280b68f2916e45239363c4fd5c9e6
@cmmdmx <me@cmmdmx.de>
2023-02-15 18:27:33 +0100

    version bump and minor improvements

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./app/App";
-import "./app/index.scss";
+import "./index.scss";
 import { Router } from "./Router";
 
-/**
- * Render the React App
- */
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <Router>
